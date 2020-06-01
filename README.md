d053f5927d3e46e60f6c7da3614b995006ef6b99
oasyslab\luca.lambiase <luca@lambia.it>
2020-06-01 02:47:33 +0200

    add electron

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# vue-overlap
+# vue-electron-app
 
 ## Project setup
 ```
@@ -15,6 +15,16 @@ yarn run serve
 yarn run build
 ```
 
+### Launch Electron Wrapper
+```
+yarn run electron
+```
+
+### Compiles and hot-reloads into Electron Wrapper for development
+```
+yarn run start
+```
+
 ### Run your tests
 ```
 yarn run test
@@ -28,5 +38,6 @@ yarn run lint
 ### .env.[NAME]
 ```
 NODE_ENV= environment name
+APP_URL= url to load into electron
 VUE_URL= vue base url path
 ```
---
+++ app.js
@@ -0,0 +1,19 @@
+require('dotenv').config({ path: '.env' })
+require('dotenv').config({ path: `.env.${process.env.ENV}` })
+console.log("Environment: ", process.env.NODE_ENV);
+
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+let url
+if (process.env.NODE_ENV === 'development') {
+    url = process.env.APP_URL
+} else {
+    url = `file://${process.cwd()}${process.env.APP_URL}`
+}
+
+app.on('ready', () => {
+    let window = new BrowserWindow({ width: 800, height: 600 })
+    window.loadURL(url)
+})
